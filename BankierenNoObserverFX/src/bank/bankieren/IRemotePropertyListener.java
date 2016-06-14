772ae5c27bdb95885987d8034fb2d17a35ecc7e3
Mario <mario.schipper@student.fontys.nl>
2016-06-14 21:33:39 +0200

    Observer toegevoegd

---
+++ BankierenNoObserverFX/src/bank/bankieren/IRemotePropertyListener.java
@@ -0,0 +1,20 @@
+package bank.bankieren;
+
+import java.beans.PropertyChangeEvent;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.EventListener;
+
+/**
+ *
+ * @author Mastermouse
+ */
+public interface IRemotePropertyListener extends EventListener, Remote {
+    
+    /**
+     * Notify all listeners subscribed to a property about a change
+     * @param evt property change details
+     * @throws RemoteException 
+     */
+    void propertyChange(PropertyChangeEvent evt) throws RemoteException;
+}
---
+++ BankierenNoObserverFX/src/bank/bankieren/IRemotePublisher.java
@@ -0,0 +1,27 @@
+package bank.bankieren;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Mastermouse
+ */
+public interface IRemotePublisher extends Remote {
+    
+    /**
+     * Subscribe a listener to a property
+     * @param listener Object implementing IRemotePropertyListener that wants to subscribe to a property
+     * @param property Property that listener wants to subscribe to
+     * @throws RemoteException 
+     */
+    void addListener(IRemotePropertyListener listener, String property) throws RemoteException;
+    
+    /**
+     * Unsubscribe a listener from a property
+     * @param listener Object implementing IRemotePropertyListener that wants to unsubscribe from a property
+     * @param property Property that the listener wants to unsubscribe from
+     * @throws RemoteException 
+     */
+    void removeListener(IRemotePropertyListener listener, String property) throws RemoteException;
+}
